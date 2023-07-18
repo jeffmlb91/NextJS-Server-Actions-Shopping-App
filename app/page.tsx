26d458b01a626165d0bb20a37f52c9f0b1d18fc3
jeffmlb91 <jeffmlb91@gmail.com>
2023-07-18 18:19:34 -0400

    add: implimented the revalidate function with next tag to update the ui as well

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { revalidateTag } from "next/cache";
 import Image from "next/image";
 
 export interface Product {
@@ -10,10 +11,38 @@ export default async function Home() {
     "https://64b492340efb99d8626913b5.mockapi.io/products",
     {
       cache: "no-cache",
+      next: {
+        tags: ["products"] //this tag will be revalidated to update the ui
+      }
     }
   );
 
   const products: Product[] = await res.json();
+
+  const addProductToDatabase = async (e: FormData) => {
+    'use server'
+    //we are grabbing the values from the form (just like we would with useState)
+    const product = e.get("product")?.toString();
+    const price = e.get("price")?.toString();
+
+    if (!product || !price) return; 
+
+    //then we will create/build a new product object for the new product to be added.(storing the values)
+    const newProduct: Product = {
+      product,
+      price
+    }
+    //then we pass the new Product to the endPoint we created in MockAPI and Make a post request and in the body strinfify the new product
+    await fetch("https://64b492340efb99d8626913b5.mockapi.io/products", {
+      method: 'POST',
+      body: JSON.stringify(newProduct),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    //We have to revalidate the tag here
+    revalidateTag('products')
+  }
 
   return (
     <main className="">
@@ -21,14 +50,14 @@ export default async function Home() {
         Products Inventory
       </h1>
 
-      <form action="" className="flex flex-col gap-5 max-w-xl mx-auto p-5">
+      <form action={addProductToDatabase} className="flex flex-col gap-5 max-w-xl mx-auto p-5">
         <input
-          type="text"
+          name="product"
           placeholder="Enter New Product Name"
           className="border border-gray  p-2 rounded-md"
         />
         <input
-          type="text"
+          name="price"
           placeholder="Enter New Product Price"
           className="border border-gray p-2 rounded-md"
         />
